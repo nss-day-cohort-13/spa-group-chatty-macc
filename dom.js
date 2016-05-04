2893d139e305c298b6bc7c7889679ef46ff6b4ac
Melanie Baker <melanierbaker@yahoo.com>
2016-05-04 11:46:03 -0500

    added eventlisteners for the clear button and the two check boxes

--- dom.js
+++ dom.js
@@ -1,8 +1,24 @@
 var userInput = document.getElementById("user-input");
 
-document.getElementById("user-input").addEventListener("keypress", function(event){
+userInput.addEventListener("keypress", function(event){
   if (event.which === 13) {
     Chatty.addMessage("message-box", userInput.value);
     userInput.value = "";
   }
 });
+
+var clearButton = document.getElementById("clear-button");
+clearButton.addEventListener("click", function(){
+  console.log("clearButton" );
+});
+
+var darkTheme = document.getElementById("dark-theme");
+darkTheme.addEventListener("change", function(){
+  console.log("darkTheme" );
+});
+
+var largeText = document.getElementById("large-text");
+largeText.addEventListener("change", function(){
+
+});
+
