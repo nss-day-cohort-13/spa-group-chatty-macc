47ee027b6c986fe1c41a06bdc92f7070fa7688a3
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-04 12:07:57 -0500

    iife-fy

--- dom.js
+++ dom.js
@@ -1,24 +1,27 @@
-var userInput = document.getElementById("user-input");
+'use strict';
 
-userInput.addEventListener("keypress", function(event){
-  if (event.which === 13) {
-    Chatty.addMessage("message-box", userInput.value);
-    userInput.value = "";
-  }
-});
+(function() {
+  var userInput = document.getElementById("user-input");
 
-var clearButton = document.getElementById("clear-button");
-clearButton.addEventListener("click", function(){
-  console.log("clearButton" );
-});
+  userInput.addEventListener("keypress", function(event){
+    if (event.which === 13) {
+      Chatty.addMessage("message-box", userInput.value);
+      userInput.value = "";
+    }
+  });
 
-var darkTheme = document.getElementById("dark-theme");
-darkTheme.addEventListener("change", function(){
-  console.log("darkTheme" );
-});
+  var clearButton = document.getElementById("clear-button");
+  clearButton.addEventListener("click", function(){
+    console.log("clearButton" );
+  });
 
-var largeText = document.getElementById("large-text");
-largeText.addEventListener("change", function(){
+  var darkTheme = document.getElementById("dark-theme");
+  darkTheme.addEventListener("change", function(){
+    console.log("darkTheme" );
+  });
 
-});
+  var largeText = document.getElementById("large-text");
+  largeText.addEventListener("change", function(){
 
+  });
+})();
