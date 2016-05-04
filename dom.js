89dd0f8fc1b9dd73da85eaf1c477a417ffdb200c
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-04 13:13:15 -0500

    Clear button event listener

--- dom.js
+++ dom.js
@@ -2,6 +2,7 @@
 
 (function() {
   var userInput = document.getElementById("user-input");
+  var messageBox = document.getElementById("message-box");
 
   userInput.addEventListener("keypress", function(event){
     if (event.which === 13) {
@@ -12,16 +13,19 @@
 
   var clearButton = document.getElementById("clear-button");
   clearButton.addEventListener("click", function(){
-    console.log("clearButton" );
+    console.log("clearButton");
+    Array.from(document.getElementsByClassName("message")).forEach(function(element) {
+      Chatty.removeElement(element.id);
+    })
   });
 
   var darkTheme = document.getElementById("dark-theme");
   darkTheme.addEventListener("change", function(){
-    console.log("darkTheme" );
+    console.log("dark theme", event.target.checked);
   });
 
   var largeText = document.getElementById("large-text");
   largeText.addEventListener("change", function(){
-
+    console.log("large text", event.target.checked);
   });
 })();
