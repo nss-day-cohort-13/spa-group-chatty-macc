e0ae31156cebdd2a2cdd550dfa465ca747a66ac2
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-05 11:17:12 -0500

    username and timestamp on message output

--- chatty-array.js
+++ chatty-array.js
@@ -8,6 +8,12 @@ var Chatty = (function(chat) {
     chat.removeElement(event.target.parentElement.id);
   }
 
+  function formatTimestamp(ts) {
+    var date = new Date(ts);
+    var iso = date.toISOString();
+    return iso.replace(/(.+)T(.+)\..+/, '$2 $1');
+  }
+
   chat.addMessage = function(parentId, message) {
     message.id = "message" + newId++;
     messages.push(message);
@@ -16,8 +22,10 @@ var Chatty = (function(chat) {
     messageElement.id = message.id;
     messageElement.className = "message";
     messageElement.innerHTML = `
+      <span class="message-user">${message.user}</span>
       <span class="message-text">${message.message}</span>
-      <input type="button" class="delete-button" value="Delete">`;
+      <input type="button" class="delete-button" value="Delete">
+      <span class="message-time">${formatTimestamp(message.timestamp)}</span>`;
     document.getElementById(parentId).appendChild(messageElement);
 
     var deleteButton = messageElement.getElementsByClassName("delete-button")[0];
